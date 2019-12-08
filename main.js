213f707a36fdaaacb4b14f8ff07a84962a66f354
Manuel Aguirre <aguirre.manuel7@gmail.com>
2019-12-08 18:31:55 +0100

    Shuffle Perfeccionado
    es el algoritmo ese de knuth

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ memotest.deck = ['carta01', 'carta01', 'carta02', 'carta02', 'carta03', 'carta03
     'carta08', 'carta08']
 
 function shuffleDeck(deck) {
-    for (let index = 1; index < deck.length; index++) {
+    
+    for (let index = deck.length - 1; index > 0; index--) {
         
-        let randomSmallerIndex = Math.floor((Math.random() * index));
-        let b = deck[index];
-        deck[index] = deck[randomSmallerIndex];
-        deck[randomSmallerIndex] = b;
+        let randomIndex = Math.floor((Math.random() * (index + 1)));
+
+        let b = deck[randomIndex];
+        deck[randomIndex] = deck[index];
+        deck[index] = b;
+
     }
 }
 
