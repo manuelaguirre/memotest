9747475b87ce256717d7555fc0e0947680e41868
Manuel Aguirre <aguirre.manuel7@gmail.com>
2019-12-11 19:33:18 +0100

    comienzo a trabajar en la lógica del juego
    trato de mejorar el reparto de cartas, también muevo el attribute data-pattern a los reversos.

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ let memotest = {};
 memotest.deck = ['carta01', 'carta01', 'carta02', 'carta02', 'carta03', 'carta03', 'carta04', 'carta04', 'carta05', 'carta05', 'carta06', 'carta06', 'carta07', 'carta07',
     'carta08', 'carta08']
 
+let diccionarioImagenes = {"carta01":"img/200px-Cristinakirchnermensaje2010.jpg",
+"carta02": "img/220px-Eduardo_duhalde_presidente.jpg",
+"carta03": "img/220px-Mauricio_Macri_2016.jpg",
+"carta04":"img/220px-Raúl_Alfonsin.jpg",
+"carta05":"img/250px-Fernando_de_la_Rúa_con_bastón_y_banda_de_presidente.jpg",
+"carta06":"img/250px-Menem_con_banda_presidencial.jpg",
+"carta07":"img/270px-Alberto_fernandez_presidente_(cropped).jpg",
+"carta08":"img/Presidente_Nestor_Kirchner_(cropped).jpg"};
+
 function shuffleDeck(deck) {
 
     for (let index = deck.length - 1; index > 0; index--) {
@@ -16,12 +25,26 @@ function shuffleDeck(deck) {
     }
 }
 
+function repartirMazo(deck, $listaCuadros){
+    //aquí se requiere que el mazo y la lista de los cuadros tengan el mismo tamaño
+    shuffleDeck(deck);
+    for (let index = 0; index < $listaCuadros.length; index++) {
+        $listaCuadros[index].setAttribute("data-pattern", deck[index]);
+        
+    }
+
+}
+
+function renderizarImagenes(){
+
+}
+
 let playGame = false;
 
 
 document.querySelectorAll('.cuadro').forEach(function($cuadro){
     $cuadro.onclick = manejarInputUsuario;
-    $cuadro.classList.add("yourvieja");
+    
 });
 
 
@@ -50,9 +73,7 @@ function manejarInputUsuario(e) {
 
 
 
-function dealCards() {
 
-}
 
 function comprobarMatch(nodeList) {
 
